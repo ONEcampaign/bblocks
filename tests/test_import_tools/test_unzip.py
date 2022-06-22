3137c71b52ed21b7f1d715daf1020663e4f87026
lpicci96 <lpicci96@gmail.com>
2022-06-22 09:57:26 -0600

    add documentation to tests

--- tests/test_import_tools/test_unzip.py
+++ tests/test_import_tools/test_unzip.py
@@ -1,25 +1,27 @@
 """tests for unzip.py"""
-
+import pandas as pd
 import pytest
 from package.import_tools.unzip import read_zipped_csv
 
-VALID_URL = 'https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/SDG.zip'
+VALID_ZIP_URL = 'https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/SDG.zip'
+VALID_URL = 'https://apiportal.uis.unesco.org/bdds'
 VALID_PATH = 'SDG_LABEL.csv'
 
-def test_valid_url_and_path():
-    """ """
-    assert len(read_zipped_csv(VALID_URL, VALID_PATH))>0
+def test_read_zipped_csv_valid():
+    """test read_zipped_csv with valid url and path"""
+    assert isinstance(read_zipped_csv(VALID_ZIP_URL, VALID_PATH), pd.DataFrame)
 
-def test_invalid_url():
-    """ """
+def test_read_zipped_csv_invalid_url():
+    """test read_zipped_csv with invalid url"""
     with pytest.raises(ValueError) as exception_info:
         read_zipped_csv("", VALID_PATH)
         assert exception_info.match('invalid url')
 
-def test_invalid_path():
-    """ """
+def test_read_zipped_csv_invalid_path():
+    """test read_zipped_csv with invalid path"""
     with pytest.raises(ValueError) as exception_info:
-        read_zipped_csv(VALID_URL, "")
+        read_zipped_csv(VALID_ZIP_URL, "")
         assert exception_info.match('invalid file path')
+
 
 
