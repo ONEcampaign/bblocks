7062416ff8f9b006c68ed46b73d607121695be29
lpicci96 <lpicci96@gmail.com>
2022-06-22 09:27:07 -0600

    create read_zipped_csv function

--- package/import_tools/unzip.py
+++ package/import_tools/unzip.py
@@ -1,5 +1,22 @@
 """ """
+import pandas as pd
+import requests
+import io
+from zipfile import ZipFile
 
-def read_zipped_csv(url: str, path: str):
+def read_zipped_csv(url: str, path: str) -> pd.DataFrame:
     """ """
-    pass
+
+    #request url and unzip file
+    try:
+        response = requests.get(url)
+        folder = ZipFile(io.BytesIO(response.content))
+
+    except ConnectionError:
+        raise ConnectionError('invalid url')
+
+    #read csv
+    if path not in list(folder.NameToInfo.keys()):
+        raise ValueError('invalid file path')
+    else:
+        return pd.read_csv(folder.open(path), low_memory=False)
