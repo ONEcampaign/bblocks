dabf93271472c23c14946d29156d3bdca7f5ce63
Jorge Rivera <jorge.rivera@one.org>
2022-06-22 11:03:59 -0600

    Docstrings

--- package/import_tools/world_bank.py
+++ package/import_tools/world_bank.py
@@ -60,6 +60,14 @@ def _get_wb_data(
 
 
 class WorldBankData(ImportData):
+    """An object to help download data from the World Bank.
+    In order to use, create an instance of this class.
+    Then, call the load_indicator method to load an indicator. This can be done multiple times.
+    If the data for an indicator has never been downloaded, it will be downloaded.
+    If it has been downloaded, it will be loaded from disk.
+    You can force an update by calling `update` if you want to refresh the data stored on disk.
+    You can get a dataframe of the data by calling `get_data`."""
+
     def load_indicator(
         self,
         indicator_code: str,
@@ -68,8 +76,19 @@ class WorldBankData(ImportData):
         end_year: Optional | int = None,
         most_recent_only: bool = False,
     ) -> WorldBankData:
-        """Load an indicator from the World Bank"""
+        """Get an indicator from the World Bank API
 
+        Args:
+            indicator_code: the code from the World Bank data portal (e.g. "SP.POP.TOTL")
+            indicator_name: A human-readable name for the indicator (e.g. "Total Population")
+            start_year: The first year to include in the data
+            end_year: The last year to include in the data
+            most_recent_only: If True, only get the most recent non-empty value for each country
+
+        Returns:
+            The same object to allow chaining
+
+        """
         years_str = (
             f"{start_year}-{end_year}"
             if all([isinstance(start_year, int), isinstance(end_year, int)])
@@ -93,8 +112,8 @@ class WorldBankData(ImportData):
             _get_wb_data(**__params).to_csv(
                 f"{PATHS.imported_data}/{file_name}", index=False
             )
-        else:
-            _ = pd.read_csv(f"{PATHS.imported_data}/{file_name}")
+
+        _ = pd.read_csv(f"{PATHS.imported_data}/{file_name}")
 
         __params["file_name"] = file_name
 
@@ -103,6 +122,15 @@ class WorldBankData(ImportData):
         return self
 
     def update(self) -> ImportData:
+        """Update the data saved on disk for the different indicators
+
+        When called, it will go through each indicator and update the data saved
+        based on the parameters passed to load_indicator.
+
+        Returns:
+            The same object to allow chaining
+
+        """
         if self.indicators is None:
             raise RuntimeError("No indicators loaded")
 
@@ -115,6 +143,17 @@ class WorldBankData(ImportData):
         return self
 
     def get_data(self, indicators: Optional | str | list = "all") -> pd.DataFrame:
+        """
+        Get the data as a Pandas DataFrame
+        Args:
+            indicators: By default, all indicators are returned in a single DataFrame.
+                If a list of indicators is passed, only those indicators will be returned.
+                A single indicator can be passed as a string as well.
+
+        Returns:
+            A Pandas DataFrame with the data for the indicators requested.
+
+        """
         if self.data is not None:
             return self.data
 
