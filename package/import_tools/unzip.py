e4479b561ec1371b821cd5da5dd37424bd997fae
lpicci96 <lpicci96@gmail.com>
2022-06-22 09:43:29 -0600

    add documentation

--- package/import_tools/unzip.py
+++ package/import_tools/unzip.py
@@ -5,7 +5,19 @@ import io
 from zipfile import ZipFile
 
 def read_zipped_csv(url: str, path: str) -> pd.DataFrame:
-    """ """
+    """Reads a CSV from a zipped file on the web
+
+    Parameters
+    ----------
+    url: str
+         url link to the zipped file
+    path: str
+          path to the csv in the folder
+    Returns
+    -------
+    pd.DataFrame
+        pandas dataframe object
+    """
 
     #request url and unzip file
     try:
@@ -20,3 +32,4 @@ def read_zipped_csv(url: str, path: str) -> pd.DataFrame:
         raise ValueError('invalid file path')
     else:
         return pd.read_csv(folder.open(path), low_memory=False)
+
