3796003a70ef63d8df48a8f810c39cf4e91310cc
Jorge Rivera <jorge.rivera@one.org>
2022-06-22 10:38:36 -0600

    update ImportData class

---
+++ package/import_tools/common.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from abc import abstractmethod, ABC
+from dataclasses import dataclass, field
+
+import pandas as pd
+
+
+@dataclass
+class ImportData(ABC):
+    indicators: dict = field(default_factory=dict)
+    data: pd.DataFrame = None
+
+    @abstractmethod
+    def load_indicator(self, **kwargs) -> ImportData:
+        """Load the data saved on disk"""
+        pass
+
+    @abstractmethod
+    def update(self, **kwargs) -> ImportData:
+        """Update object data"""
+        pass
+
+    @abstractmethod
+    def get_data(self, **kwargs) -> pd.DataFrame:
+        """Get the data as a Pandas DataFrame"""
+        pass
