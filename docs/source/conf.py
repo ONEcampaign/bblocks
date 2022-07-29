ca0b0057cf9a3f745a4ac03ce57b4b1b19ece2eb
Jorge Rivera <jorge.rivera@one.org>
2022-07-29 09:07:07 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../bblocks/'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 project = 'bblocks'
 copyright = '2022, The ONE Campaign'
