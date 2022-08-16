fad455cfe8e742c3e09333a008e6824ac7b62d67
Jorge Rivera <jorge.rivera@one.org>
2022-08-17 00:01:18 +0200

    added optional aggregation

--- bblocks/dataframe_tools/change.py
+++ bblocks/dataframe_tools/change.py
@@ -1,16 +1,20 @@
+from typing import Optional
+
 import pandas as pd
 
-from bblocks.cleaning_tools.clean import to_date_column
+from bblocks.cleaning_tools.clean import to_date_column, convert_id
 
 
 def _resample_group(
     group,
     *,
+    group_by: list,
     date_column: str,
     freq: str,
+    aggregation: str | None,
     keep_latest: int | None = None,
 ):
-    """Helper function to take the data in a given 'group' (a Pandas dataframe) and
+    """Helper function which take the data in a given 'group' (a Pandas dataframe) and
     resample it to the specified frequency."""
 
     # If keep latest not None, then an offset by latest N values.
@@ -27,8 +31,22 @@ def _resample_group(
     mask = group.reset_index(drop=True).index.isin(offset)
     df_latest, df_other = group.loc[mask], group.loc[~mask]
 
-    # Resample other data
-    df_r = df_other.set_index(date_column).asfreq(freq=freq).reset_index(drop=False)
+    df_latest = df_latest.drop(columns=group_by)
+
+    if aggregation is None:
+        df_r = (
+            df_other.set_index(date_column)
+            .asfreq(freq=freq)
+            .drop(columns=group_by)
+            .reset_index(drop=False)
+        )
+
+    else:
+        df_r = df_other.resample(rule=freq, on=date_column).agg(aggregation)
+        try:
+            df_r = df_r.reset_index(drop=False)
+        except ValueError:
+            df_r = df_r.reset_index(drop=True).drop(columns=group_by)
 
     return pd.concat([df_r, df_latest], ignore_index=True).drop_duplicates(keep="first")
 
@@ -38,6 +56,7 @@ def date_resample_to(
     date_column: str,
     group_by: list,
     to_freq: str = "W-MON",
+    aggregation: Optional[str | None] = None,
     keep_latest: int | None = None,
 ) -> pd.DataFrame:
     """Resample a dataframe to a specified frequency."""
@@ -46,16 +65,23 @@ def date_resample_to(
     if not pd.api.types.is_datetime64_any_dtype(df[date_column]):
         df[date_column] = to_date_column(df[date_column])
 
+    # Save list of original columns
+    columns = df.columns
+
     # Create analytical groups
     df_ = (
         df.copy(deep=True)
-        .groupby(group_by, as_index=False)
+        .groupby(group_by)
         .apply(
             _resample_group,
+            group_by=group_by,
             date_column=date_column,
             freq=to_freq,
+            aggregation=aggregation,
             keep_latest=keep_latest,
         )
+        .reset_index(drop=False)
+        .filter(columns, axis=1)
     )
     # Return reordering columns and sorting by date
     return df_.sort_values(by=[date_column] + group_by).reset_index(drop=True)
