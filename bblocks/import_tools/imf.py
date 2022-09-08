1adad55fdfe5fb5d794ac658ed73e330b16935a2
lpicci96 <lpicci96@gmail.com>
2022-09-08 18:16:45 +0200

    update formatting

--- bblocks/import_tools/imf.py
+++ bblocks/import_tools/imf.py
@@ -77,9 +77,9 @@ def _read_sdr_tsv(url: str) -> pd.DataFrame:
 
     return (
         df.melt(id_vars="member", value_vars=["holdings", "allocations"])
-            .pipe(clean_numeric_series, series_columns="value")
-            .rename(columns={"variable": "indicator"})
-            .reset_index(drop=True)
+        .pipe(clean_numeric_series, series_columns="value")
+        .rename(columns={"variable": "indicator"})
+        .reset_index(drop=True)
     )
 
 
@@ -173,7 +173,7 @@ class SDR(ImportData):
         indicator_list = _check_sdr_indicators(indicator)
 
         if not os.path.exists(f"{PATHS.imported_data}/{self.file_name}") or (
-                self.update_data and self.data is None
+            self.update_data and self.data is None
         ):
             self.update(reload_data=False)
 
@@ -239,9 +239,9 @@ class SDR(ImportData):
         return self
 
     def get_data(
-            self,
-            indicators: str = "all",
-            members: Optional[str | list] = "all",
+        self,
+        indicators: str = "all",
+        members: Optional[str | list] = "all",
     ) -> pd.DataFrame:
         """Get the data as a Pandas DataFrame
 
@@ -290,7 +290,7 @@ class SDR(ImportData):
 
 
 def _check_weo_parameters(
-        latest_y: int | None = None, latest_r: int | None = None
+    latest_y: int | None = None, latest_r: int | None = None
 ) -> (int, int):
     """Check parameters and return max values or provided input"""
     if latest_y is None:
@@ -321,7 +321,7 @@ class WorldEconomicOutlook(ImportData):
     """World Economic Outlook data"""
 
     def __load_data(
-            self, latest_y: int | None = None, latest_r: int | None = None
+        self, latest_y: int | None = None, latest_r: int | None = None
     ) -> None:
         """loading WEO as a clean dataframe
 
@@ -352,8 +352,8 @@ class WorldEconomicOutlook(ImportData):
 
         # If data doesn't exist or update is required, update the data
         if (
-                not os.path.exists(f"{PATHS.imported_data}/weo{latest_y}_{latest_r}.csv")
-                or self.update_data
+            not os.path.exists(f"{PATHS.imported_data}/weo{latest_y}_{latest_r}.csv")
+            or self.update_data
         ):
             _update_weo(latest_y, latest_r)
 
@@ -363,14 +363,14 @@ class WorldEconomicOutlook(ImportData):
         # Load data into data object
         self.data = (
             df.drop(to_drop, axis=1)
-                .rename(columns=names)
-                .melt(id_vars=names.values(), var_name="year", value_name="value")
-                .assign(
+            .rename(columns=names)
+            .melt(id_vars=names.values(), var_name="year", value_name="value")
+            .assign(
                 year=lambda d: pd.to_datetime(d.year, format="%Y"),
                 value=lambda d: clean_numeric_series(d.value),
             )
-                .dropna(subset=["value"])
-                .reset_index(drop=True)
+            .dropna(subset=["value"])
+            .reset_index(drop=True)
         )
 
     def _check_indicators(self, indicators: str | list | None = None) -> None | dict:
@@ -381,9 +381,9 @@ class WorldEconomicOutlook(ImportData):
         # Create dictionary of available indicators
         indicators_ = (
             self.data.drop_duplicates(subset=["indicator", "indicator_name", "units"])
-                .assign(name_units=lambda d: d.indicator_name + " (" + d.units + ")")
-                .set_index("indicator")["name_units"]
-                .to_dict()
+            .assign(name_units=lambda d: d.indicator_name + " (" + d.units + ")")
+            .set_index("indicator")["name_units"]
+            .to_dict()
         )
 
         if indicators is None:
@@ -398,7 +398,7 @@ class WorldEconomicOutlook(ImportData):
                 raise ValueError(f"Indicator not found: {_}")
 
     def load_indicator(
-            self, indicator_code: str, indicator_name: Optional[str] = None
+        self, indicator_code: str, indicator_name: Optional[str] = None
     ) -> ImportData:
         """Loads a specific indicator from the World Economic Outlook data"""
 
@@ -410,7 +410,7 @@ class WorldEconomicOutlook(ImportData):
 
         self.indicators[indicator_code] = (
             self.data.loc[lambda d: d.indicator == indicator_code]
-                .assign(
+            .assign(
                 indicator_name=indicator_name
                 if indicator_name is not None
                 else lambda d: d.indicator_name,
@@ -419,9 +419,9 @@ class WorldEconomicOutlook(ImportData):
                     axis=1,
                 ),
             )
-                .drop(columns=["estimates_start_after"])
-                .sort_values(["iso_code", "year"])
-                .reset_index(drop=True)
+            .drop(columns=["estimates_start_after"])
+            .sort_values(["iso_code", "year"])
+            .reset_index(drop=True)
         )
         return self
 
@@ -446,7 +446,7 @@ class WorldEconomicOutlook(ImportData):
         print(f"Available indicators:\n{''.join(available)}")
 
     def get_data(
-            self, indicators: str | list = "all", keep_metadata: bool = False
+        self, indicators: str | list = "all", keep_metadata: bool = False
     ) -> pd.DataFrame:
 
         df = _get_data(obj=self, indicators=indicators)
@@ -464,10 +464,10 @@ def __get_rate(rows: list, text: str):
         if text in rows[i].text:
             return float(rows[i + 1].text.strip().split("\r\n")[0])
     else:
-        raise ValueError('Could not find exchange rate')
+        raise ValueError("Could not find exchange rate")
 
 
-def latest_sdr_exchange(currency: Optional | str = 'USD') -> dict[str:str, str:float]:
+def latest_sdr_exchange(currency: Optional | str = "USD") -> dict[str:str, str:float]:
     """Extracts the latest SDR exchange rate and date
 
     Args:
@@ -495,10 +495,10 @@ def latest_sdr_exchange(currency: Optional | str = 'USD') -> dict[str:str, str:f
 
     rows = table.find_all("td")
 
-    if currency == 'USD':
-        exchange_info.update({'value': __get_rate(rows, 'U.S.$1.00 = SDR')})
-    elif currency == 'SDR':
-        exchange_info.update({'value': __get_rate(rows, 'SDR1 = US$')})
+    if currency == "USD":
+        exchange_info.update({"value": __get_rate(rows, "U.S.$1.00 = SDR")})
+    elif currency == "SDR":
+        exchange_info.update({"value": __get_rate(rows, "SDR1 = US$")})
     else:
         raise ValueError('Invalid currency. Please select from ["SDR", "USD]')
 
