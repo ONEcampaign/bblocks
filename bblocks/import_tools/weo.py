1b1be01da55c375722dc95c62c5c81281fc9b8bf
Luca Picci <lpicci96@gmail.com>
2022-12-01 11:46:36 +0100

    add formatting

--- bblocks/import_tools/weo.py
+++ bblocks/import_tools/weo.py
@@ -1,4 +1,5 @@
 """Tools to extract data from the World Economic Outlook database."""
+import xml.etree.ElementTree
 
 import pandas as pd
 import requests
@@ -6,7 +7,6 @@ import xml.etree.ElementTree as ET
 from zipfile import ZipFile
 import io
 from bs4 import BeautifulSoup
-
 
 BASE = 'https://www.imf.org/'
 
@@ -74,12 +74,69 @@ def parse_xml(root) -> pd.DataFrame:
     return pd.DataFrame(rows)
 
 
-def get_weo(year: int, month: str) -> pd.DataFrame:
+def get_schema_root(folder):
+    """Get the root of the xml file"""
+
+    tree = ET.parse(folder.open(folder.namelist()[1]))
+    return tree.getroot()
+
+
+def _get_mapper(root: xml.etree.ElementTree.Element, lookup_value: str):
+    """get mapper for a column"""
+
+    query = root.findall('./{http://www.w3.org/2001/XMLSchema}simpleType[@name='
+                         + f'"{lookup_value}"'
+                         + ']/')[0].findall('./')
+    return {elem.attrib['value']: elem[0][0].text for elem in query}
+
+
+def convert_series_codes(root: xml.etree.ElementTree.Element,
+                         series: pd.Series,
+                         lookup_value: str) -> pd.Series:
+    """Convert codes to values in a series"""
+
+    mapper = _get_mapper(root, lookup_value)
+    return series.map(mapper)
+
+
+def convert_codes(root: xml.etree.ElementTree.Element, df: pd.DataFrame) -> pd.DataFrame:
+    """" """
+
+    column_mapper = {'UNIT': 'IMF.CL_WEO_UNIT.1.0',
+                     'CONCEPT': 'IMF.CL_WEO_CONCEPT.1.0',
+                     'REF_AREA': 'IMF.CL_WEO_REF_AREA.1.0',
+                     'FREQ': 'IMF.CL_FREQ.1.0',
+                     'SCALE': 'IMF.CL_WEO_SCALE.1.0'
+
+                     }
+
+    for col, value in column_mapper.items():
+        df = df.rename(columns = {col: f'{col}_CODE'})
+        df[col] = convert_series_codes(root, df[f'{col}_CODE'], value)
+
+    return df
+
+
+def extract_weo(year: int, month: str) -> ZipFile:
     """Retrieve WEO data for a given year and month"""
 
     href = get_sdmx_href(year, month)
-    folder = get_folder(BASE + href)
+    return get_folder(BASE + href)
+
+
+def read_weo(folder):
+    """ """
+
     root = get_root(folder)
     return parse_xml(root)
+
+
+def get_weo(year: int, month: str):
+    """ """
+
+    folder = extract_weo(year, month)
+    df = read_weo(folder)
+    schema_root = get_schema_root(folder)
+    return convert_codes(schema_root, df)
 
 
