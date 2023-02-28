465b1c583056df577a60f3e1339e9dc468179925
Luca Picci <lpicci96@gmail.com>
2023-02-28 15:08:44 +0100

    update documentation

--- bblocks/import_tools/ilo.py
+++ bblocks/import_tools/ilo.py
@@ -12,7 +12,7 @@ from bblocks.config import BBPaths
 BASE_URL = "https://www.ilo.org"
 
 
-def _get_glossaries_links(lang: str = 'en') -> dict[str, str]:
+def _get_glossaries_links(lang: str = "en") -> dict[str, str]:
     """Get reference dictionaries for codes and names of indicators
 
     Args:
@@ -24,20 +24,22 @@ def _get_glossaries_links(lang: str = 'en') -> dict[str, str]:
     """
     d = {}
 
-    response = requests.get(f'{BASE_URL}/ilostat-files/WEB_bulk_download/html/bulk_dic.html')
-    soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(
+        f"{BASE_URL}/ilostat-files/WEB_bulk_download/html/bulk_dic.html"
+    )
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    for a in soup.find_all('a'):
-        if f'_{lang}.csv' in a.text:
-            d.update({a.text.split('_')[0]: f"{BASE_URL}/{a.get('href')}"})
+    for a in soup.find_all("a"):
+        if f"_{lang}.csv" in a.text:
+            d.update({a.text.split("_")[0]: f"{BASE_URL}/{a.get('href')}"})
 
     if len(d) == 0:
-        raise ValueError(f'Language {lang} not available. Choose from en, fr, es')
+        raise ValueError(f"Language {lang} not available. Choose from en, fr, es")
 
     return d
 
 
-def get_glossaries(lang: str = 'en') -> dict[str, dict[str, str]]:
+def get_glossaries(lang: str = "en") -> dict[str, dict[str, str]]:
     """Get reference dictionaries for codes and names of indicators
 
     Args:
@@ -53,7 +55,7 @@ def get_glossaries(lang: str = 'en') -> dict[str, dict[str, str]]:
     for name, link in glossaries_links.items():
         d.update({name: pd.read_csv(link, index_col=0).iloc[:, 0].to_dict()})
 
-    d.update({'obs_status': d.pop('obs')})
+    d.update({"obs_status": d.pop("obs")})
 
     return d
 
@@ -67,17 +69,16 @@ def extract_data(indicator_code: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Dataframe with the data
 
-    Exceptions:
-        requests.exceptions.HTTPError: If the indicator is not valid
-
     """
 
-    url = f"{BASE_URL}/ilostat-files/WEB_bulk_download/indicator/{indicator_code}.csv.gz"
+    url = (
+        f"{BASE_URL}/ilostat-files/WEB_bulk_download/indicator/{indicator_code}.csv.gz"
+    )
 
     try:
-        return pd.read_csv(url, compression='gzip')
+        return pd.read_csv(url, compression="gzip")
     except requests.exceptions.HTTPError:
-        raise requests.exceptions.HTTPError(f'Indicator {indicator_code} not available')
+        raise requests.exceptions.HTTPError(f"Indicator {indicator_code} not available")
 
 
 def clean_df(df, glossaries) -> pd.DataFrame:
@@ -89,24 +90,42 @@ def clean_df(df, glossaries) -> pd.DataFrame:
 
     Returns:
         pd.DataFrame: Cleaned dataframe
-
     """
 
-    # df = df.drop(columns=['note_classif', 'note_source']) # drop unwanted columns
     # loop through glossaries and rename columns
     for key in glossaries:
         if key in df.columns:
-            df = (df.rename(columns = {key: f'{key}_code'})) # rename columns to *_code
-            df[key] = df[f'{key}_code'].map(glossaries[key]) # add new column with glossary names
+            df = df.rename(columns={key: f"{key}_code"})  # rename columns to *_code
+            df[key] = df[f"{key}_code"].map(
+                glossaries[key]
+            )  # add new column with glossary names
 
     return df
 
 
 @dataclass
 class ILO(ImportData):
-    """An object to import data from the ILO website"""
+    """An object to import data from the ILO website
+
+    To use this object, first create an instance of it and then call the `load_data` method
+    to load an indicator to the object. Use the `get_data` method to get the data from the object.
+    To update the data saved on disk and in the object, call the `update_data` method.
+    The attribute `available_indicators` contains a dataframe with information on
+    all the available indicators from the ILO.
+    """
 
     _glossaries: dict = None
+    _available_indicators: pd.DataFrame = None
+
+    @property
+    def available_indicators(self) -> pd.DataFrame:
+        """Return a datafra"""
+
+        if self._available_indicators is None:
+            self._available_indicators = pd.read_csv(
+                f"{BASE_URL}//ilostat-files/WEB_bulk_download/indicator/table_of_contents_en.csv"
+            )
+        return self._available_indicators
 
     def _load_glossaries(self):
         """Load the glossaries to the object"""
@@ -114,12 +133,23 @@ class ILO(ImportData):
         self._glossaries = get_glossaries()
 
     def load_data(self, indicator: str | list) -> ImportData:
-        """Load the data saved on disk"""
+        """Load an ILO indicator to the object
+
+        This method with check if the indicator is already saved to disk. If not, it
+        will downlaod it to disk and load it to the object. Use the `get_data` method
+        to get the data from the object after it is loaded.
+
+        Args:
+            indicator (str | list): Indicator code or list of indicator codes to load
+
+        Returns:
+            ImportData: The object with the data loaded
+        """
 
         if isinstance(indicator, str):
             indicator = [indicator]
 
-        for ind in indicator: # loop through indicators
+        for ind in indicator:  # loop through indicators
 
             path = BBPaths.raw_data / f"{indicator}.csv"
 
@@ -131,18 +161,30 @@ class ILO(ImportData):
                     self._load_glossaries()
 
                 # download data
-                (extract_data(ind)
-                 .pipe(clean_df, self._glossaries)
-                 .to_csv(path, index=False)
-
-                 )
+                (
+                    extract_data(ind)
+                    .pipe(clean_df, self._glossaries)
+                    .to_csv(path, index=False)
+                )
 
             # load data to object
             self._data[ind] = pd.read_csv(path)
             return self
 
     def update_data(self, reload_data: bool = True) -> ImportData:
-        """ """
+        """Update the data saved on disk
+
+        When called it will go through all the indicators loaded to the object
+        and download the data again. If reload_data is True, it will also reload the data
+        to the object.
+
+        Args:
+            reload_data: If True, it will reload the data to the object. Defaults to True.
+
+        Returns:
+            ImportData: The object with the updated data
+
+        """
 
         if len(self._data) == 0:
             raise RuntimeError("No indicators loaded")
@@ -151,20 +193,17 @@ class ILO(ImportData):
         if self._glossaries is None:
             self._load_glossaries()
 
-        for ind in self._data: # loop through loaded indicators
+        for ind in self._data:  # loop through loaded indicators
 
             # extract data
-            (extract_data(ind)
-             .pipe(clean_df, self._glossaries)
-             .to_csv(BBPaths.raw_data / f"{ind}.csv", index=False)
-             )
+            (
+                extract_data(ind)
+                .pipe(clean_df, self._glossaries)
+                .to_csv(BBPaths.raw_data / f"{ind}.csv", index=False)
+            )
 
             # reload data to object if reload_data is True
             if reload_data:
                 self._data[ind] = pd.read_csv(BBPaths.raw_data / f"{ind}.csv")
 
         return self
-
-
-        
-
