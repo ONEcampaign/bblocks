25c7aa8491ce5d4284f227258ad93c0df8e5fb41
Luca Picci <lpicci96@gmail.com>
2026-01-28 12:18:13 +0100

    replace loading logic

--- src/bblocks/data_importers/baci/baci.py
+++ src/bblocks/data_importers/baci/baci.py
@@ -6,6 +6,78 @@ from bblocks.data_importers.config import Fields, logger
 from bblocks.data_importers.baci.extract import extract_data_links, BaciDataManager
 
 
+def _add_product_labels(data_manager: BaciDataManager) -> None:
+    """Add product labels to the data DataFrame
+
+    Returns:
+        DataFrame with product labels added
+    """
+
+    prod_mapping = (data_manager._product_codes
+                    .set_index(Fields.product_code)
+                    [Fields.product_description]
+                    .to_dict()
+                    )
+
+    data_manager._data = (data_manager._data
+                          .assign(**{Fields.product_description: lambda d: d[Fields.product_code].map(prod_mapping)})
+                          )
+
+def _add_country_labels(data_manager: BaciDataManager) -> None:
+    """Add country labels to the data DataFrame including country name and ISO3 code
+
+    Returns:
+        DataFrame with country labels added
+    """
+
+    country_name_mapping = (data_manager._country_codes
+                            .set_index(Fields.country_code)
+                            [Fields.country_name]
+                            .to_dict()
+                            )
+
+    iso3_mapping = (data_manager._country_codes
+                    .set_index(Fields.country_code)
+                    [Fields.iso3_code]
+                    .to_dict()
+                    )
+
+    data_manager._data = (data_manager._data
+            .assign(**{
+                Fields.exporter_name: lambda d: d[Fields.exporter_code].map(country_name_mapping),
+                Fields.importer_name: lambda d: d[Fields.importer_code].map(country_name_mapping),
+                Fields.exporter_iso3_code: lambda d: d[Fields.exporter_code].map(iso3_mapping),
+                Fields.importer_iso3_code: lambda d: d[Fields.importer_code].map(iso3_mapping),
+            })
+            )
+
+def _load_data(hs_version: str) -> BaciDataManager:
+    """Helper function to load data for a specific HS version
+    Verify the correct HS version is provided, and ensure the data link is available.
+    """
+
+    data_links = extract_data_links()
+
+    if hs_version not in list(data_links.keys()):
+        raise ValueError(f"HS version {hs_version} not available. "
+                         f"Available versions: {list(data_links.keys())}")
+
+    logger.info(f"Extracting BACI data for HS version {hs_version}")
+
+    data_manager = BaciDataManager(hs_version=hs_version, url=data_links[hs_version])
+    data_manager.extract_zip_file() # extract data
+
+    logger.info("Parsing BACI data files")
+
+    data_manager.read_data() # read data
+    _add_product_labels(data_manager) # add product labels
+    _add_country_labels(data_manager) # add country labels
+
+    # TODO: validation checks
+
+    logger.info(f"Successfully extracted BACI data for HS version {hs_version}")
+
+    return data_manager
 
 class BACI:
     """BACI data importer class"""
@@ -24,83 +96,28 @@ class BACI:
 
         return list(self._hs_versions.keys())
 
-    def _load_data(self, hs_version) -> None:
-        """Helper function to load data for a specific HS version
-        Verify the correct HS version is provided, and ensure the data link is available.
-        """
-
-        if hs_version not in self.available_hs_versions():
-            raise ValueError(f"HS version {hs_version} not available. "
-                             f"Available versions: {list(self._hs_versions.keys())}")
-
-        self._data[hs_version] = BaciDataManager(hs_version=hs_version, url=self._hs_versions[hs_version])
-        self._data[hs_version].load_data()
-
-    def _add_product_labels(self, df: pd.DataFrame, hs_version: str) -> pd.DataFrame:
-        """Add product labels to the data DataFrame
-
-        Returns:
-            DataFrame with product labels added
-        """
-
-        logger.info("Adding product labels")
-
-        prod_mapping = (self._data[hs_version]._product_codes
-                        .set_index(Fields.product_code)
-                        [Fields.product_description]
-                        .to_dict()
-                        )
-
-        return (df
-                .assign(**{Fields.product_description: lambda d: d[Fields.product_code].map(prod_mapping)})
-                )
-
-    def _add_country_labels(self, df: pd.DataFrame, hs_version: str) -> pd.DataFrame:
-        """Add country labels to the data DataFrame including country name and ISO3 code
-
-        Returns:
-            DataFrame with country labels added
-        """
-
-        logger.info("Adding country labels")
-
-        country_name_mapping = (self._data[hs_version]._country_codes
-                                .set_index(Fields.country_code)
-                                [Fields.country_name]
-                                .to_dict()
-                                )
-
-        iso3_mapping = (self._data[hs_version]._country_codes
-                        .set_index(Fields.country_code)
-                        [Fields.iso3_code]
-                        .to_dict()
-                        )
-
-        return (df
-                .assign(**{
-                    Fields.exporter_name: lambda d: d[Fields.exporter_code].map(country_name_mapping),
-                    Fields.importer_name: lambda d: d[Fields.importer_code].map(country_name_mapping),
-                    Fields.exporter_iso3_code: lambda d: d[Fields.exporter_code].map(iso3_mapping),
-                    Fields.importer_iso3_code: lambda d: d[Fields.importer_code].map(iso3_mapping),
-                })
-                )
-
     def get_data(self, hs_version: str = "HS22",
                  include_product_labels: bool = False,
-                 include_country_labels: bool = False) -> pd.DataFrame:
+                 include_country_labels: bool = False
+                 ) -> pd.DataFrame:
         """ """
 
         # if data for the requested HS version is not loaded, load it
         if hs_version not in self._data:
-            self._load_data(hs_version)
+            self._data[hs_version] = _load_data(hs_version)
 
         df = self._data[hs_version]._data.copy(deep=True)
 
-        if include_product_labels:
-            df = self._add_product_labels(df, hs_version)
+        if not include_product_labels:
+            df = df.drop(columns=[Fields.product_description])
 
-        if include_country_labels:
-            df = self._add_country_labels(df, hs_version)
+        if not include_country_labels:
+            df = df.drop(columns=[
+                Fields.exporter_name,
+                Fields.importer_name,
+                Fields.exporter_iso3_code,
+                Fields.importer_iso3_code,
+            ])
 
         return df
 
